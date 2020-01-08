4297c7691547b4cb2d23e3885633e550a1520a74
Johan Öbrink <johan.obrink@gmail.com>
2020-01-08 16:47:21 +0100

    feat: 🎸 Added messages for reading and writing recipients (#67)

--- __test__/schemas.test.js
+++ __test__/schemas.test.js
@@ -111,4 +111,51 @@ describe('schemas', () => {
       })
     })
   })
+  describe('RECIPIENTS_READ_RESPONSE', () => {
+    it('validates a correct payload', async () => {
+      const payload = {
+        ...jwtDefaults,
+        type: 'RECIPIENTS_READ_RESPONSE',
+        sub: 'ccec677d-09d1-489a-a3da-e4758134f2fa',
+        paths: [
+          {
+            domain: 'http://cv.work',
+            area: 'education',
+            recipients: [
+              {
+                header: {
+                  kid: 'http://192.168.110.179:4000/jwks/uD-zoYsfaDuu45sJCuWi3ScZ-raoF3YsEFQcgF4zjmQ',
+                  alg: 'RSA-OAEP'
+                },
+                encrypted_key: 'r0f49P7BY_sscD9n7VYZ1mIKlwwGCXIh-Ep6X7mfddr76PIOwrkHkk4kps5uvUEQ1mqfV1h4-eYpCwPFW6jTPXwfSp4iULH211ygp6iuesB5-kZaMsqAZ1ZkVJPkMAikKZT9IayuAF3giRFTcuGAxiBaq9sUE57fOma8wS7WXSiPa35KbedYRETWQ6vCX5uXdftAL22pE5FuueHimTNqB2wgRiagiiMKDec96HLC3gf7zacM60nrwO3Az5KHb-sgIZv9ZOwG4yiPJK2MczI1kQDYk4mIb_MK6ad4zV_wzXLBQTPWIm2_7oIxUYaTqpkPh42ss-mspoVwM4pJ6fjs2A'
+              },
+              {
+                header: {
+                  kid: 'egendata://jwks/cu6sJNkWsYmEr3OoTc3V12Bdln1hcVMVt3lQ6scFNtM',
+                  alg: 'RSA-OAEP'
+                },
+                encrypted_key: 'FyoGL5D25xa-jJKB4OYTOeuktnKwnO3hMeh1bW1qwpkTYS317Dn2SoBIWQyMMTbH1uOv_F6oO6tqVqZUlixQgAu65To15ARSHHQflKtzVMDhIMSgQ1vFjZkqy5AJQEoaEcNbbGORzcj5nM0M5PwNFGTRqBbXN4e9HcOIo_u0jZO0li3HY7bo6_IfUrS0ZDd1N2N29KFY__J47ZMk0pG0q5yWYcC9cqpB6tY9xCAO0QWX8-P6aVUaOMHKb4AH_g4jx0eh7kFQkG29snqTFkSkuE9_lab9xki3UZCtOvAIhHtligD4HYm1YkuUAtA7p-q-ojJovVRSPBkesXjKuyYJUA'
+              }
+            ]
+          },
+          {
+            domain: 'http://cv.work',
+            area: 'education',
+            recipients: [
+              {
+                header: {
+                  kid: 'egendata://jwks/cu6sJNkWsYmEr3OoTc3V12Bdln1hcVMVt3lQ6scFNtM',
+                  alg: 'RSA-OAEP'
+                },
+                encrypted_key: 'FyoGL5D25xa-jJKB4OYTOeuktnKwnO3hMeh1bW1qwpkTYS317Dn2SoBIWQyMMTbH1uOv_F6oO6tqVqZUlixQgAu65To15ARSHHQflKtzVMDhIMSgQ1vFjZkqy5AJQEoaEcNbbGORzcj5nM0M5PwNFGTRqBbXN4e9HcOIo_u0jZO0li3HY7bo6_IfUrS0ZDd1N2N29KFY__J47ZMk0pG0q5yWYcC9cqpB6tY9xCAO0QWX8-P6aVUaOMHKb4AH_g4jx0eh7kFQkG29snqTFkSkuE9_lab9xki3UZCtOvAIhHtligD4HYm1YkuUAtA7p-q-ojJovVRSPBkesXjKuyYJUA'
+              }
+            ]
+          }
+        ]
+      }
+      await expect(
+        schemas.RECIPIENTS_READ_RESPONSE.validate(payload)
+      ).resolves.not.toThrow()
+    })
+  })
 })
