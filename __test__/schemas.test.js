cc42d17998aac699963d0c3f0ebcd44e764801a4
Mikael Gråborg <mikael.graborg@iteam.se>
2020-05-28 14:40:48 +0200

    chore: change schema to allow recipients read resp.
    also add more detailed req to CONNECTION schema obj

--- __test__/schemas.test.js
+++ __test__/schemas.test.js
@@ -63,6 +63,15 @@ describe('schemas', () => {
         sid: 'sdkfhdkskdfd',
         sub: 'baa949aa-fbb5-4aad-8351-d6ef219dd07b',
         permissions: {
+          denied: [
+            {
+              id: '392c6472-40e4-4e2b-92e2-77a46c1900b8',
+              domain: 'https://mycv.work',
+              area: 'edumacation',
+              type: 'WRITE',
+              lawfulBasis: 'CONSENT'
+            }
+          ],
           approved: [
             {
               id: '91910133-4024-4641-a7c7-91fb6e11588e',
@@ -91,24 +100,69 @@ describe('schemas', () => {
         }
       }
     })
+
     it('validates a correct payload', async () => {
       await expect(
         schemas.CONNECTION.validate(connection)
       ).resolves.not.toThrow()
     })
-    describe('CONNECTION_RESPONSE', () => {
-      it('validates a correct payload', async () => {
-        const payload = {
-          ...jwtDefaults,
-          aud: 'https://smoothoperator',
-          type: 'CONNECTION_RESPONSE',
-          payload:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoiYWRtaW4iLCJpYXQiOjE0MjI3Nzk2Mzh9.gzSraSYS8EXBxLN_oWnFSRgCzcmJmMjLiuyu5CSpyHI'
-        }
-        await expect(
-          schemas.CONNECTION_RESPONSE.validate(payload)
-        ).resolves.not.toThrow()
-      })
+
+    it('validates a correct payload without any permissions', async () => {
+      delete connection.permissions
+      await expect(
+        schemas.CONNECTION.validate(connection)
+      ).resolves.not.toThrow()
+    })
+    
+    it('throws if permissions exists but is empty', async () => {
+      connection.permissions = {}
+      await expect(
+        schemas.CONNECTION.validate(connection)
+      ).rejects.toThrow()
+    })
+
+    // This would should be an invalid object
+    // Checking if values are undefined is not possible in joi. 
+    //  Hopefully and probably, it will not cause any problems when parsing
+    it.skip('throws if permission.denied exists but is undefined', async () => {
+      connection.permissions.denied = undefined
+      await expect(
+        schemas.CONNECTION.validate(connection)
+      ).rejects.toThrow()
+    })
+
+    // This would should be an invalid object
+    // Checking if values are undefined is not possible in joi. 
+    //  Hopefully and probably, it will not cause any problems when parsing
+    it.skip('throws if permission.approved exists but is undefined', async () => {
+      connection.permissions.approved = undefined
+      await expect(
+        schemas.CONNECTION.validate(connection)
+      ).rejects.toThrow()
+    })
+
+    // This would should be an invalid object
+    // Checking if values are undefined is not possible in joi. 
+    //  Hopefully and probably, it will not cause any problems when parsing
+    it.skip('throws if permissions exists but is undefined', async () => {
+      connection.permissions = undefined
+      await expect(
+        schemas.CONNECTION.validate(connection)
+      ).rejects.toThrow()
+    })
+  })
+  describe('CONNECTION_RESPONSE', () => {
+    it('validates a correct payload', async () => {
+      const payload = {
+        ...jwtDefaults,
+        aud: 'https://smoothoperator',
+        type: 'CONNECTION_RESPONSE',
+        payload:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoiYWRtaW4iLCJpYXQiOjE0MjI3Nzk2Mzh9.gzSraSYS8EXBxLN_oWnFSRgCzcmJmMjLiuyu5CSpyHI'
+      }
+      await expect(
+        schemas.CONNECTION_RESPONSE.validate(payload)
+      ).resolves.not.toThrow()
     })
   })
   describe('RECIPIENTS_READ_RESPONSE', () => {
